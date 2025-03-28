676998ab22d627093c501b452f2583bf4c5f51ce
Samuel Sant Anna <samuelsantaanna@gmail.com>
2025-03-28 19:30:15 -0300

    few fixes in ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import characters from "../../players.json"
 
 const charactersData = characters;
 const App: React.FC = () => {
-  console.log(characters[0]);
   return (
     <>
       <div className='topContainer'>
         <div className='session-content'>
           <h1>Here comes the content of the current session </h1>
         </div>
+        <div></div>
         <div className='characters'>
           {charactersData.map((character) => (
             <CharacterSheet characterData={character} />
