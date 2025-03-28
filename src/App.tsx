f1ce0e885d3e62a3ec4142ad45f236437e280337
Samuel Sant Anna <samuelsantaanna@gmail.com>
2025-03-28 19:54:16 -0300

    lots of tests... some stay

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import { CharacterSheet } from './components/CharacterSheet';
-import characters from "../../players.json"
+import characters from "../players.json"
 
 const charactersData = characters;
 const App: React.FC = () => {
+
+  // const handleClick: any (() => { })
   return (
     <>
       <div className='topContainer'>
         <div className='session-content'>
           <h1>Here comes the content of the current session </h1>
+          {/* <button onClick={handleClick}></button> */}
         </div>
         <div></div>
         <div className='characters'>
